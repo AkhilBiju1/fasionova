a057916c492f2ac9c20e0dd51282dd447744dc91
AkhilBiju1 <106715011+AkhilBiju1@users.noreply.github.com>
2023-03-26 19:46:12 +0530

    Update connetion.js

--- config/connetion.js
+++ config/connetion.js
@@ -4,7 +4,7 @@ local = "mongodb://127.0.0.1:27017/"
 const atls ='mongodb+srv://mern-fasionova-user:myfirstproject.com@cluster0.mdrwgoj.mongodb.net/mern-fasionova-db?retryWrites=true&w=majority'
 module.exports= {
     connect : ()=> {
-        const url = local
+        const url = atls
         database = "shopping"
         
         mongoose.connect(url + database, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
